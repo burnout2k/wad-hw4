9b263eb1985ee8ccdce5b8e26e4799a841d03681
Allan Mitt <allan.mitt@gmail.com>
2019-12-13 06:29:12 +0200

    test 3 done

--- tests/unit/Dummy.spec.js
+++ tests/unit/Dummy.spec.js
@@ -2,7 +2,9 @@
 import { mount } from '@vue/test-utils'
 import List from "../../src/components/List.vue";
 import Header from "../../src/components/Header.vue";
-describe('List component', () => {
+import Footer from "../../src/components/Footer.vue";
+
+describe('display a message when there are no tasks in the list', () => {
 
     const testData = []
     const wrapper = mount(List, {
@@ -24,17 +26,28 @@ describe('Header is rendered with correct day/month/year/weekday', () => {
 
         let months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT',	'NOV', 'DEC'];
         let weekdays = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
-
         let today = new Date();
-
         let day = String(today.getDate()).padStart(2, '0');
+
         expect(wrapper.html()).toContain("<h1>"+day+"</h1>")
-
         expect(wrapper.html()).toContain("<h4 class=\"strong\">"+months[today.getMonth()]+"</h4>")
-
         expect(wrapper.html()).toContain("<h4>"+today.getFullYear()+"</h4>")
-
         expect(wrapper.html()).toContain("<h4>"+weekdays[today.getDay()]+"</h4>")
 
+    })
+});
+
+describe(' when "+" button is clicked in Footer component "open" property in Footer component changes to true', () => {
+
+    const wrapper = mount(Footer)
+
+    it('updates open status when button is clicked', async () => {
+
+        expect(wrapper.vm.open).toEqual(false);
+        //console.log(wrapper.vm.open)
+        wrapper.find('span').trigger('click')
+        //await wrapper.vm.$nextTick()
+        expect(wrapper.vm.open).toEqual(true);
+        //console.log(wrapper.vm.open)
     })
 });
